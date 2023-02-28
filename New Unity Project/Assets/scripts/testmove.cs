69c06d612b2bfe19e8d3370a5b2b5cd2cb0f11e5
Yann Malaret <yann.malaret@epitech.eu>
2023-02-28 14:37:37 +0100

    tilemap, test base mouv, sprites

---
+++ New Unity Project/Assets/scripts/testmove.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testmove : MonoBehaviour
+{
+    private CharacterController controller;
+
+    private Vector3 move;
+
+    [SerializeField] private float speed = 3f;
+    [SerializeField] private float rotationSpeed = 100f;
+    [SerializeField] private float jumpHeight = 3f;
+    [SerializeField] private float gravity = -9.81f;
+    private float jumpVelocity = 0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool grounded = controller.isGrounded;
+        if (grounded && jumpVelocity < 0)
+        {
+            jumpVelocity = 0;
+        }
+        
+        //translation
+        move = new Vector3(x: 0, y: 0, z: Input.GetAxis("Vertical")) * speed;
+        move = transform.TransformDirection(move);
+        controller.Move(motion: move * Time.deltaTime);
+        
+        //rotation
+        transform.Rotate(eulers:Time.deltaTime * rotationSpeed * new Vector3(x:0, y:Input.GetAxis("Horizontal"), z:0));
+        
+        //saut
+        if (grounded && Input.GetKey(name: "space"))
+        {
+            jumpVelocity += Mathf.Sqrt(f: jumpHeight * -3.0F * gravity);
+        }
+        jumpVelocity += gravity * Time.deltaTime;
+        controller.Move(motion: new Vector3(x: 0, y: jumpVelocity, z: 0) * Time.deltaTime);
+    }
+}
---
+++ tilemap test/Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    private Rigidbody2D ribo;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        ribo = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float dirX = Input.GetAxisRaw("Horizontal");
+        ribo.velocity = new Vector2(dirX * 4f, ribo.velocity.y);
+        if (Input.GetKeyDown("space"))
+        {
+            ribo.velocity = new Vector2(ribo.velocity.x, 5f);
+        }
+    }
+}
